02d12c100ccf1db39e4bbe957dcbc72f1f2f4a59
John <write2johnjoseph@gmail.com>
2022-02-26 20:39:26 +0530

    3 questions from the final term question sheet.

---
+++ FinalPrep2022/loop2.java
@@ -0,0 +1,23 @@
+package FinalPrep2022;
+import java.util.*;
+public class loop2 {
+    public static void main(String[] args) {
+        //WAP to input 10 numbers print the sum of odd and even numbers separately.
+        Scanner obj = new Scanner(System.in);
+        int num = 0;
+        int odd = 0;
+        int even = 0;
+        for(int i=1;i<=10;i++){
+            System.out.println("Enter a number: ");
+            num = obj.nextInt();
+            if(num%2==0){
+                even+=num;
+            } else if(num%2==1){
+                odd+=num;
+            }
+        }
+        System.out.println("Addition of all odd numbers: " + odd);
+        System.out.println("Addition of all even numbers: " + even);
+        obj.close();
+    }
+}
---
+++ FinalPrep2022/loop3.java
@@ -0,0 +1,16 @@
+package FinalPrep2022;
+import java.util.*;
+public class loop3 {
+    public static void main(String[] args) {
+        //WAP to input a number and print its first 10 multiples.
+        Scanner obj = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Enter a number: ");
+        num = obj.nextInt();
+        for(int i=1;i<=10;i++){
+            int multiple = num*i;
+            System.out.println("Multiple number "+i+": "+multiple);
+        }
+        obj.close();
+    }
+}
