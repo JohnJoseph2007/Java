fafc0963e1ec6ca77446d9f1fea85acdaf2ffbbb
John <write2johnjoseph@gmail.com>
2022-04-25 00:41:05 +0530

    :)

--- specialnumber.java
+++ specialnumber.java
@@ -2,21 +2,26 @@ import java.util.*;
 class SpecialNumber {
     public static void main(String[] args){
         Scanner obj = new Scanner(System.in);
-        int n = 0;
+        int n = 0, x=0;
         int factorial = 1;
         int sums = 0;
         System.out.println("Enter a number: ");
         n = obj.nextInt();
-        for(int i = n; i!=0; i%=10){
-            for(int j = i; j!=0; j--){
-                factorial*=j;
+        int number = n;
+        while(number>0){
+            x = number%10;
+            for(int i = 1; i<=x; i++){
+                factorial*=i;
             }
             sums+=factorial;
+            number/=10;
+            factorial=1;
         }
         if(sums==n){
             System.out.println(n + " is a special number.");
         } else {
             System.out.println(n + " is not a special number.");
         }
+        obj.close();
     }
 }
