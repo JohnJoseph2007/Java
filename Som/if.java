182fa80bfaa5cd46ab3735102fdc6c7556270064
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2022-06-11 13:22:00 +0530

    ;;

---
+++ Som/if.java
@@ -0,0 +1,14 @@
+class HiSom {
+    public static void main(String[] args){
+        int a = 69;
+        int b = 70;
+        if(a>b){ 
+            //if a is greater than b, it will enter
+            System.out.println("A is greater than B.");
+        } else if(b>a) {
+            System.out.println("B is greater than A.");
+        } else {
+
+        }
+    }
+}
